d530f26bfe9ce6b7515b0599bc2e549adf8c4cff
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-03-11 23:23:50 +0700

    -

--- platform/themes/main/vue/core/layouts/Content.vue
+++ platform/themes/main/vue/core/layouts/Content.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-content">
         <a-layout-content :style="{ margin: '15px 15px 0' }">
-            <div :style="{ padding: '15px', background: '#fff', minHeight: 'calc(100vh - 120px)' }">
+            <div :style="{ padding: '15px', background: '#fff', height: 'calc(100vh - 120px)' }">
                 <slot name="content"></slot>
             </div>
         </a-layout-content>
--- platform/themes/main/vue/modules/Message/Index.vue
+++ platform/themes/main/vue/modules/Message/Index.vue
@@ -2,7 +2,71 @@
     <has-sidebar-layout>
         <Content>
             <template v-slot:content>
-                <h1>Tin nhắn</h1>
+                <div class="message-wrapper h100">
+                    <div class="message__box">
+                        <div class="message__box__left">
+                            <div class="user__list">
+                                <div v-for="i in 5" :key="i" class="user__item">
+                                    <div class="user__item-avatar">
+                                        <img src="https://picsum.photos/200/300" alt="User" />
+                                    </div>
+                                    <div class="user__item-lasted-message">
+                                        <div class="user__name">Donal Trump</div>
+                                        <div class="lasted__message">Hello! How are you ?</div>
+                                    </div>
+                                    <div class="user__item__timer">1 giờ</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="message__box__right">
+                            <div class="message__box__header">
+                                <div class="message__box__header-user">
+                                    <div class="user-avatar">
+                                        <img src="https://picsum.photos/200/300" alt="Avatar" />
+                                    </div>
+                                    <div class="user-name">
+                                        <div class="name">Donal Trump</div>
+                                        <div class="status"><span class="dot green"></span>Online</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="message__box__content">
+                                <div class="message__partner node_message">
+                                    <div class="partner_avatar node_message_avatar">
+                                        <img src="https://picsum.photos/200/300" alt="User" />
+                                    </div>
+                                    <div class="message__list node_message_list">
+                                        <div class="message__item node_message_item"><p>sadsa</p></div>
+                                        <div class="message__item node_message_item"><p>sdsad sdsad</p></div>
+                                        <div class="message__item node_message_item">
+                                            <p>sdsad sadsad sadsad</p>
+                                            <p class="time"><span>21:46</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="message__author node_message">
+                                    <div class="author_avatar node_message_avatar">
+                                        <img src="https://picsum.photos/200/300" alt="User" />
+                                    </div>
+                                    <div class="message__list node_message_list">
+                                        <div class="message__item node_message_item"><p>abc</p></div>
+                                        <div class="message__item node_message_item"><p>def</p></div>
+                                        <div class="message__item node_message_item recent__message">
+                                            <p>ádassafsa</p>
+                                            <p class="time"><span>21:46</span><span>Đã gửi</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="message__form">
+                                <div class="message__form__input">
+                                    <a-input class="input" size="large" />
+                                    <a-button :htmlType="'button'" type="primary" size="large">Send</a-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </template>
         </Content>
     </has-sidebar-layout>
@@ -15,7 +79,7 @@ import Content from '@core/layouts/Content.vue';
 export default {
     components: {
         HasSidebarLayout,
-        Content
+        Content,
     },
     data() {
         return {
