a45b133e8b972e9892e759f8522df6011749728c
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-03-11 12:50:53 +0700

    -

--- platform/themes/main/vue/core/vendor/validate.js
+++ platform/themes/main/vue/core/vendor/validate.js
@@ -1,54 +1,53 @@
-import Vue from 'vue'
-import { ValidationProvider, extend, ValidationObserver } from 'vee-validate'
-import { required, email, numeric, min, max, max_value, min_value } from 'vee-validate/dist/rules'
+import Vue from "vue";
+import { ValidationProvider, extend, ValidationObserver } from "vee-validate";
+import {
+    required,
+    email,
+    numeric,
+    min,
+    max,
+    max_value,
+    min_value
+} from "vee-validate/dist/rules";
 
-extend('password', {
-    params: ['target'],
+extend("password", {
+    params: ["target"],
     validate(value, { target }) {
-      return value === target;
-    },
-    message: 'Xác nhận mật khẩu chưa chính xác'
-  });
+        return value === target;
+    }
+});
 
-extend('required', {
-    ...required,
-    message: 'Trường thông tin này là bắt buộc'
-})
+extend("required", {
+    ...required
+});
 
-extend('email', {
-    ...email,
-    message: 'Email sai định dạng'
-})
+extend("email", {
+    ...email
+});
 
-extend('numeric', {
-    ...numeric,
-    message: 'Trường thông tin này phải là một số'
-})
+extend("numeric", {
+    ...numeric
+});
 
-extend('min', {
+extend("min", {
     ...min,
-    params:  ['length'],
-    message: 'Trường thông tin này có tối thiểu {length} ký tự'
-})
+    params: ["length"]
+});
 
-extend('max', {
+extend("max", {
     ...max,
-    params:  ['length'],
-    message: 'Trường thông tin này có tối đa {length} ký tự'
-})
+    params: ["length"]
+});
 
-extend('min_value', {
+extend("min_value", {
     ...min_value,
-    params:  ['value'],
-    message: 'Trường này phải ít hơn hoặc bằng {value} ký tự'
-})
+    params: ["value"]
+});
 
-extend('max_value', {
+extend("max_value", {
     ...max_value,
-    params:  ['value'],
-    message: 'Trường này phải nhiều hơn hoặc bằng {value} ký tự'
-})
+    params: ["value"]
+});
 
-
-Vue.component('ValidationProvider', ValidationProvider)
-Vue.component('ValidationObserver', ValidationObserver)
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
--- platform/themes/main/vue/modules/Auth/Index.vue
+++ platform/themes/main/vue/modules/Auth/Index.vue
@@ -1,28 +1,38 @@
 <template>
     <no-sidebar-layout>
         <div class="login-wrapper">
-            <div class="form login__form">
-                <div class="form__title">Login</div>
-                <div class="form__group">
-                    <div class="form__group-label required"><span>Email</span></div>
-                    <div class="form__group-input">
-                        <a-input size="large" placeholder="Email" />
+            <ValidationObserver v-slot="{ handleSubmit }">
+                <form @submit.prevent="handleSubmit(handleLogin)">
+                    <div class="form login__form">
+                        <div class="form__title">Login</div>
+                        <div class="form__group">
+                            <div class="form__group-label required"><span>Email</span></div>
+                            <div class="form__group-input">
+                                <ValidationProvider name="Email" rules="required|email" v-slot="{ errors }">
+                                    <a-input v-model="formData.email" size="large" placeholder="Email" />
+                                    <span class="error-message" v-show="errors[0]">{{ errors[0] }}</span>
+                                </ValidationProvider>
+                            </div>
+                        </div>
+                        <div class="form__group">
+                            <div class="form__group-label required"><span>Password</span></div>
+                            <div class="form__group-input">
+                                <ValidationProvider name="Password" rules="required" v-slot="{ errors }">
+                                    <a-input-password v-model="formData.password" size="large" placeholder="Password" />
+                                    <span class="error-message" v-show="errors[0]">{{ errors[0] }}</span>
+                                </ValidationProvider>
+                            </div>
+                        </div>
+                        <div class="form__hyperlink">
+                            <router-link :to="{ name: 'login' }"><a-icon type="user-add" /> Register</router-link>
+                            <router-link :to="{ name: 'login' }"><a-icon type="lock" /> Forgot password?</router-link>
+                        </div>
+                        <div class="form__action">
+                            <a-button :htmlType="'submit'" size="large" icon="login" :loading="loading" type="primary">Login</a-button>
+                        </div>
                     </div>
-                </div>
-                <div class="form__group">
-                    <div class="form__group-label required"><span>Password</span></div>
-                    <div class="form__group-input">
-                        <a-input-password size="large" placeholder="Password" />
-                    </div>
-                </div>
-                <div class="form__hyperlink">
-                    <router-link :to="{ name: 'login' }"><a-icon type="user-add" /> Register</router-link>
-                    <router-link :to="{ name: 'login' }"><a-icon type="lock" /> Forgot password?</router-link>
-                </div>
-                <div class="form__action">
-                    <a-button size="large" icon="login" :loading="loading" type="primary">Login</a-button>
-                </div>
-            </div>
+                </form>
+            </ValidationObserver>
         </div>
     </no-sidebar-layout>
 </template>
@@ -36,7 +46,19 @@ export default {
     data() {
         return {
             loading: false,
+            formData:{
+                email : '',
+                password : ''
+            }
         };
+    },
+    methods: {
+        handleLogin() {
+            this.loading = true;
+            setTimeout(() => {
+                this.loading = false;
+            }, 3000);
+        },
     },
 };
 </script>
