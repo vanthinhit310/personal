e291d38282c27d25ac092afa979ddb23cdae807e
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-04-25 23:22:57 +0700

    -

--- platform/themes/main/vue/modules/Google/Pages/GooglePhoto/GooglePhotoApi.vue
+++ platform/themes/main/vue/modules/Google/Pages/GooglePhoto/GooglePhotoApi.vue
@@ -9,15 +9,18 @@
                     </a-col>
 
                     <a-col :span="24">
-                        <masonry :cols="3" :gutter="30">
+                        <masonry :cols="{default: 5, 1000: 4, 700: 3, 400: 1}" :gutter="{default: '5px'}">
                             <div v-for="(item, index) in imgsArr" :key="index">
-                                <a class="d-block" data-fancybox="google-photo-images" :href="`${item}=w2048-h1024`">
-                                    <img class="img-fluid" :src="item" alt="Image">
+                                <a class="d-block mansory-image" data-fancybox="google-photo-images" :href="`${item.baseUrl}=w2048-h1024`">
+                                    <img class="img-fluid" :src="_.get(item,'baseUrl')" alt="Image">
                                 </a>
                             </div>
                         </masonry>
                     </a-col>
                     <a-col :span="24" style="text-align: center">
+                        <video controls>
+                            <source src="https://lh3.googleusercontent.com/lr/AFBm1_ZYBpBQw3W1-gVghDYUWknPtHR9D03f1g7LwFFwBfsqY8M_9ScHo_R_zF2DRCzREvJFGMz74AvaWbeMg-DjwyHAdm6uccbIBReOQL_lRjh54cb3tB0Tkh8GNDae4yVoMkHZ_fagMYY_QXfZz4iPXIkzkDbx3XU6okunTUG6bvNNz8Xmmly3Eq6_-jZAYAtcWpSnbdp_TrnPcRrXoHsD2dDPzCvO_Z_bN-mPmNMVVnpjLQFaJPE54u8wDX6yh90K2ciMbCatwRqoHJMwDnSlr8kBBFnEhr9t6o5JwsTb0H-6KPNxO0_wtYy34iSeQf28ZYyrx9ZG3EpGE3WUZZaxW-wFb1fK00SJvr6d4CPlRPlp5Y6W9SfQv7J6vGYGmtAELk3ixNuohEa9ANYM_YM7gqR0UBDNHtrddt-zR0HPBUh-q0k3w8SkePAOV0nSv0CDkkVnHAwW1dIMZBxvK4UVctVwBHpotdnDHwTLt3pMO9sXsv4h0lOHNz71_VK72d0jjWWSVkIPn8ulpS1nlkl1hG1fG5M_lCck_xtcsBlx3Uw8VniM_LTa44z2tIXEkP96kvS1M5gAyYGMbMqON_uusiFbQMHEwgrpif_duZtlP7tsn_bwLb2MAyLuufa-9oCVr18HZAv8E0EfvdomqUY83xH3Z0kWs-5rk5DMsIkPJQcZVeSxPupM8nPhel2PmfdT1r8zEBKpu9gWd8jUJ6mWhzNo9r6FmENqlR-_5nBsLeKIvJmUcR0nX0hgGqjDnhyvh8iICbWWEtdWK0TFc4IM8HGLFx5-DiOid0-n1hxa6xpZ0bYzBNpjJRtOIiuH0-bPwx34TrZ1Inuhclo-zQM"/>
+                        </video>
                         <el-button :native-type="'button'" type="primary" icon="el-icon-plus" @click="getMoreMedia" :loading="moreLoading">Show more</el-button>
                     </a-col>
                 </a-row>
@@ -106,10 +109,10 @@ export default {
                     const {mediaItems} = response.data
                     const {nextPageToken} = response.data
                     if (!!mediaItems) {
-                        const appendImgs = mediaItems.map(function (o) {
-                            return o.baseUrl;
-                        });
-                        this.imgsArr = [...imgsArr, ...appendImgs]
+                        // const appendImgs = mediaItems.map(function (o) {
+                        //     return o.baseUrl;
+                        // });
+                        this.imgsArr = [...imgsArr, ...mediaItems]
                     }
 
                     if (!!nextPageToken) {
